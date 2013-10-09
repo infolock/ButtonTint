94e8d90e8cff3d609e66f257fb22f2ffd87b029a
Jonathon Hibbard <jhibbard@gmail.com>
2013-10-09 01:55:48 -0400

    Create UIImage+RenderingMode.h

---
+++ UIImage+RenderingMode.h
@@ -0,0 +1,12 @@
+//
+//  UIImage+RenderingMode.h
+//
+//  Created by Jonathon Hibbard on 9/6/13.
+//  Copyright (c) 2013 Integrated Events. All rights reserved.
+//
+
+@interface UIImage (RenderingMode)
+
++(UIImage *)imageWithRenderingMode:(NSString *)imageName;
++(UIImage *)imageWithRenderingModeFromImage:(UIImage *)image;
+@end
