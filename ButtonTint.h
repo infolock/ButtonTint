e6ddc5be8ceef25976a7d1b228d6930da76cea9c
Jonathon Hibbard <jhibbard@gmail.com>
2013-10-09 01:56:48 -0400

    Create ButtonTint.h

---
+++ ButtonTint.h
@@ -0,0 +1,12 @@
+//
+//  ButtonTint.h
+//
+//  Created by Jonathon Hibbard on 9/7/13.
+//  Copyright (c) 2013 Integrated Events. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ButtonTint : UIButton
+
+@end
