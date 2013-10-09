15bb17bad5b34448df263e8166b91daaf449d628
Jonathon Hibbard <jhibbard@gmail.com>
2013-10-09 01:56:17 -0400

    Create ImageViewRenderMode.h

---
+++ ImageViewRenderMode.h
@@ -0,0 +1,12 @@
+//
+//  ImageViewRenderMode.h
+//
+//  Created by Jonathon Hibbard on 9/7/13.
+//  Copyright (c) 2013 Integrated Events. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewRenderMode : UIImageView
+
+@end
